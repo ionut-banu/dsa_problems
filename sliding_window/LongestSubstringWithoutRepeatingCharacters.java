1ab3d1e1a41bf3f42ff579e87b4f09ddb7e941d0
Ionut Banu <ionutbanu094@gmail.com>
2025-02-16 21:45:36 +0200

    Add LongestSubstringWithoutRepeatingCharacters

---
+++ sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+package sliding_window;
+
+class LongestSubstringWithoutRepeatingCharacters {
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int start = 0;
+            int end = 0;
+            int maxLength = 0;
+
+            Set<Character> uniqueChars = new HashSet<>();
+
+            while (end < s.length()) {
+                if (!uniqueChars.contains(s.charAt(end))) {
+                    uniqueChars.add(s.charAt(end));
+                    end++;
+                    if (uniqueChars.size() > maxLength) {
+                        maxLength = uniqueChars.size();
+                    }
+                } else {
+                    uniqueChars.remove(s.charAt(start));
+                    start++;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
